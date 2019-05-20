24a4c78b139356b2f931b36169fb8be873fc541a
RoryLetteney <rory.letteney@gmail.com>
2019-05-20 17:05:55 -0500

    Refactored add/subtract/multiply to accept arrays

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -3,20 +3,56 @@
 const arithmetic = require('../lib/arithmetic.js');
 
 describe('Add', () => {
-  it('This should return a + b', () => {
-    expect(arithmetic.add(2,2)).toBe(4);
+  it('This should return null', () => {
+    expect(arithmetic.add(2,2)).toBe(null);
+  });
+});
+
+describe('Add', () => {
+  it('This should return the sum', () => {
+    expect(arithmetic.add([2,2])).toBe(4);
+  });
+});
+
+describe('Add', () => {
+  it('This should return the sum without the non-number', () => {
+    expect(arithmetic.add([2,2,'hello'])).toBe(4);
   });
 });
 
 describe('Subtract', () => {
-  it('This should return a - b', () => {
-    expect(arithmetic.subtract(2,2)).toBe(0);
+  it('This should return the null', () => {
+    expect(arithmetic.subtract(2,2)).toBe(null);
+  });
+});
+
+describe('Subtract', () => {
+  it('This should return the difference', () => {
+    expect(arithmetic.subtract([2,2])).toBe(0);
+  });
+});
+
+describe('Subtract', () => {
+  it('This should return the difference without the non-number', () => {
+    expect(arithmetic.subtract([2,2,'hello'])).toBe(0);
   });
 });
 
 describe('Multiply', () => {
-  it('This should return a * b', () => {
-    expect(arithmetic.multiply(2,2)).toBe(4);
+  it('This should return null', () => {
+    expect(arithmetic.multiply(2,2)).toBe(null);
+  });
+});
+
+describe('Multiply', () => {
+  it('This should return the product', () => {
+    expect(arithmetic.multiply([2,2])).toBe(4);
+  });
+});
+
+describe('Multiply', () => {
+  it('This should return the product without the non-number', () => {
+    expect(arithmetic.multiply([2,2,'hello'])).toBe(4);
   });
 });
 
@@ -27,7 +63,7 @@ describe('Divide', () => {
 });
 
 describe('Divide', () => {
-  it('This should return a / b', () => {
+  it('This should return null', () => {
     expect(arithmetic.divide(2,0)).toBe(null);
   });
 });
