ddfc5985276f766cc03b8478756c540d4682c3f3
RoryLetteney <rory.letteney@gmail.com>
2019-05-20 17:13:06 -0500

    Externalized type checking

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
 const greet = require('./lib/greet.js');
 const math = require('./lib/arithmetic.js');
 // console.log(greet('JOHN'));
-console.log(math.add(1,3)); // 4
-console.log(math.subtract(1,3)); // -2
+console.log(math.add([1,3])); // 4
+console.log(math.subtract([1,3])); // -2
+console.log(math.multiply([1,3])); // 3
+console.log(math.divide(1,3)); // 1/3
