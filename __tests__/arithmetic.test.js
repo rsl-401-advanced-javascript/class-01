029b01322f3f39d2a5890b70b24aa39b4528c07d
RoryLetteney <rory.letteney@gmail.com>
2019-05-20 16:41:45 -0500

    Built arithmetic tests and multiply/divide functions

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -2,3 +2,32 @@
 
 const arithmetic = require('../lib/arithmetic.js');
 
+describe('Add', () => {
+  it('This should return a + b', () => {
+    expect(arithmetic.add(2,2)).toBe(4);
+  });
+});
+
+describe('Subtract', () => {
+  it('This should return a - b', () => {
+    expect(arithmetic.subtract(2,2)).toBe(0);
+  });
+});
+
+describe('Multiply', () => {
+  it('This should return a * b', () => {
+    expect(arithmetic.multiply(2,2)).toBe(4);
+  });
+});
+
+describe('Divide', () => {
+  it('This should return a / b', () => {
+    expect(arithmetic.divide(2,2)).toBe(1);
+  });
+});
+
+describe('Divide', () => {
+  it('This should return a / b', () => {
+    expect(arithmetic.divide(2,0)).toBe(null);
+  });
+});
