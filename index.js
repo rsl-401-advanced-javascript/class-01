4a8d7978805398f9e212583f29530ab87def3675
RoryLetteney <rory.letteney@gmail.com>
2019-05-20 16:22:29 -0500

    Package.json setup

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 const greet = require('./lib/greet.js');
 const math = require('./lib/arithmetic.js');
-console.log(greet('JOHN'));
+// console.log(greet('JOHN'));
 console.log(math.add(1,3)); // 4
 console.log(math.subtract(1,3)); // -2
